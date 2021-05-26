60436a0015f87e5768b4384ccb5ee2eaecc7c1b9
canutellmemyname <79779196+canutellmemyname@users.noreply.github.com>
2021-05-26 11:49:59 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "CLIMATE";
+const accessCode2 = "LIBRARY";
+const accessCode3 = "CLOUD";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("L E T I M A C", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("R B L Y R I A", 700,140)
+    fill("lightblue")
+    text("Hint: Stores all Information !!", 700,160)
+    fill("white")
+    textSize(15)
+    text("U C O D L", 300,250)
+    fill("lightblue")
+    text("Hint: Never stands at a particular space !!", 300,270)
+
+}
